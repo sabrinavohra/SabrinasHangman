ab230b760b139807bb551b499a46803eb70d34f9
sabrinavohra <sabrina.vohra@menloschool.org>
2024-04-17 11:55:43 -0700

    Created project

---
+++ src/HangMan.java
@@ -0,0 +1,16 @@
+public class HangMan {
+    // Choose a random word from the dictionary
+    // Draw the right number of underlines
+    // Let user press a letter for guessing a letter
+    // Let user press a letter for guessing the entire word
+    // Tell user if that's right or not
+    // Add to body, if necessary
+    // Check if body is completed
+    // Check if user has won
+    private String word;
+    private HangManViewer front;
+
+    public HangMan() {
+        //word = dictionary.
+    }
+}
---
+++ src/HangManViewer.java
@@ -0,0 +1,2 @@
+public class HangManViewer {
+}
